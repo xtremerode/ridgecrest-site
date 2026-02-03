b9817a3823c0e86a732e153dc292373b3c31e643
xtremerode <jasperjack.8666@gmail.com>
2026-02-02 22:04:58 -0800

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,2 @@
+const nextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: 'www.ridgecrestdesigns.com' }, { protocol: 'https', hostname: 'static.wixstatic.com' }] } };
+export default nextConfig;
